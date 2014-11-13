b527a5dce07f41cac0b5df640f6da6531a0fd79e
Augusto Roman <aroman@flux.io>
2014-11-12 23:38:15 -0800

    move chars to another file

---
+++ chars.go
@@ -0,0 +1,203 @@
+package main
+
+import "github.com/augustoroman/serial_lcd"
+
+func heart_and_snowman(lcd LCD) {
+	lcd.CreateCustomChar(0, serial_lcd.MakeChar([8]string{
+		".....",
+		".*.*.",
+		"*.*.*",
+		"*...*",
+		"*...*",
+		".*.*.",
+		"..*..",
+		".....",
+	}))
+	lcd.CreateCustomChar(1, serial_lcd.MakeChar([8]string{
+		"..o..",
+		".o.o.",
+		".ooo.",
+		"o...o",
+		".ooo.",
+		"o...o",
+		"o...o",
+		".ooo.",
+	}))
+}
+
+func invaderChars2(lcd LCD) {
+	lcd.CreateCustomChar(0, serial_lcd.MakeChar([8]string{
+		"..#..",
+		"#..#.",
+		"#.###",
+		"###.#",
+		"#####",
+		".####",
+		"..#..",
+		".#...",
+	}))
+
+	lcd.CreateCustomChar(1, serial_lcd.MakeChar([8]string{
+		"..#..",
+		".#..#",
+		"###.#",
+		"#.###",
+		"#####",
+		"####.",
+		"..#..",
+		"...#.",
+	}))
+
+	lcd.CreateCustomChar(3, serial_lcd.MakeChar([8]string{
+		"..#..",
+		"...#.",
+		"..###",
+		"###.#",
+		"#.###",
+		"#.###",
+		"..#..",
+		"...#.",
+	}))
+
+	lcd.CreateCustomChar(4, serial_lcd.MakeChar([8]string{
+		"..#..",
+		".#...",
+		"###..",
+		"#.###",
+		"###.#",
+		"###.#",
+		"..#..",
+		".#...",
+	}))
+}
+
+func watcherChars(lcd LCD) {
+	a1 := serial_lcd.MakeChar([8]string{
+		".....",
+		".....",
+		".....",
+		".....",
+		".....",
+		".....",
+		".*.*.",
+		"*.*.*",
+	})
+
+	a2 := serial_lcd.MakeChar([8]string{
+		"**...",
+		"..*..",
+		"...*.",
+		"**.*.",
+		"*..**",
+		"...**",
+		"**.*.",
+		"...*.",
+	})
+
+	a3 := serial_lcd.MakeChar([8]string{
+		"...**",
+		"..*..",
+		".*...",
+		".*.**",
+		"**..*",
+		"**...",
+		".*.**",
+		".*...",
+	})
+
+	lcd.CreateCustomChar(0, a1)
+	lcd.CreateCustomChar(1, a2)
+	lcd.CreateCustomChar(2, a3)
+}
+
+func pacmanChars(lcd LCD) {
+	a1 := serial_lcd.MakeChar([8]string{
+		".....",
+		"**...",
+		"***..",
+		"****.",
+		"****.",
+		"*****",
+		"*....",
+		".....",
+	})
+	a2 := serial_lcd.MakeChar([8]string{
+		".....",
+		"...**",
+		"..***",
+		".****",
+		"*****",
+		"*****",
+		"*****",
+		"*****",
+	})
+	a3 := serial_lcd.MakeChar([8]string{
+		".....",
+		"*....",
+		"*****",
+		"****.",
+		"***..",
+		"**...",
+		".....",
+		".....",
+	})
+	a4 := serial_lcd.MakeChar([8]string{
+		"*****",
+		"*****",
+		"*****",
+		".****",
+		"..***",
+		"...**",
+		".....",
+		".....",
+	})
+	b1 := serial_lcd.MakeChar([8]string{
+		".....",
+		".....",
+		".....",
+		".....",
+		".....",
+		".....",
+		"....*",
+		"...**",
+	})
+	b2 := serial_lcd.MakeChar([8]string{
+		".....",
+		".....",
+		".....",
+		".....",
+		".....",
+		".....",
+		"*....",
+		"**...",
+	})
+	b3 := serial_lcd.MakeChar([8]string{
+		"**...",
+		"*....",
+		".....",
+		".....",
+		".....",
+		".....",
+		".....",
+		".....",
+	})
+	b4 := serial_lcd.MakeChar([8]string{
+		"...**",
+		"....*",
+		".....",
+		".....",
+		".....",
+		".....",
+		".....",
+		".....",
+	})
+
+	lcd.CreateCustomChar(0, a1)
+	lcd.CreateCustomChar(1, a2)
+	lcd.CreateCustomChar(2, a3)
+	lcd.CreateCustomChar(3, a4)
+	lcd.CreateCustomChar(4, b1)
+	lcd.CreateCustomChar(5, b2)
+	lcd.CreateCustomChar(6, b3)
+	lcd.CreateCustomChar(7, b4)
+}
